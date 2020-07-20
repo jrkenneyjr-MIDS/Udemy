783dc96a6196cda78271bd758f1021082145527a
jrkenneyjr-MIDS <jrkenneyjr@gmail.com>
2020-07-20 11:24:54 -0500

    Django Updates

--- DjangoFullStack/CourseContent/Django/django_advanced_cbv/advcbv/basic_app/models.py
+++ DjangoFullStack/CourseContent/Django/django_advanced_cbv/advcbv/basic_app/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.urls import reverse
-
 # Create your models here.
 class School(models.Model):
     name = models.CharField(max_length=256)
@@ -8,15 +7,15 @@ class School(models.Model):
     location = models.CharField(max_length=256)
 
     def __str__(self):
-        return self.name
+        return f'ID: {self.id}\nName: {self.name}'
 
     def get_absolute_url(self):
-        return reverse("basic_app:detail",kwargs={'pk':self.pk})
+        return reverse('basic_app:detail', kwargs={'pk':self.pk})
 
 class Student(models.Model):
-    name = models.CharField(max_length=256)
+    name = models.CharField(max_length = 256)
     age = models.PositiveIntegerField()
-    school = models.ForeignKey(School,related_name='students', on_delete=models.CASCADE)
+    school = models.ForeignKey(School, related_name='students', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
